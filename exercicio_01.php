5b24d4cd1cb5beaa941b236d2a1977ce4b47067e
hiqueux <henrique_tonioti@estudante.sesisenai.org.br>
2026-07-06 08:49:12 -0300

    feat - realização do exercicio 01

---
+++ exercicio_01.php
@@ -0,0 +1,21 @@
+<?php
+
+// (x² + y²) / (x + y)
+
+function calculateFormula($x, $y)
+{
+    if (($x + $y == 0) {
+        return "Não é possivel realizar a divisão por zero.";
+    }
+
+    $resultado = (pow($x, 2) + pow($y, 2)) / ($x + $y);
+
+    return $resultado;
+}
+
+$x = 10;
+$y = 5;
+
+echo "Valor de x: " . $x . "<br>";
+echo "Valor de y: " . $y . "<br>";
+echo "Resultado da fórmula: " . calculateFormula($x, $y);
