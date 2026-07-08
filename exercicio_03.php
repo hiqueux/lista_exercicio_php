a56bdcc666b273035fdc013ae1a55a07e96bbb22
hiqueux <henrique_tonioti@estudante.sesisenai.org.br>
2026-07-08 09:02:27 -0300

    feat - realização do exercicio 03

---
+++ exercicio_03.php
@@ -0,0 +1,12 @@
+<?php
+
+function mascararCpf($cpf) {
+
+$cpfMascarado = str_repeat("*", strlen($cpf) - 4) . substr($cpf, - 4);
+return $cpfMascarado;
+
+}
+
+$cpf = "12345678900";
+echo "CPF original: $cpf<br>";
+echo "CPF mascarado: " . mascararCpf($cpf) . "<br>";
